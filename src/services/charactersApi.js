98c965b2c7c8404d025251b3fc3358292bfd339a
vicblanc92 <vicbr92@gmail.com>
2022-02-19 12:38:13 +0100

    change in URL

--- src/services/charactersApi.js
+++ src/services/charactersApi.js
@@ -1,5 +1,5 @@
 const getCharactersFromApi = (house) => {
-  return fetch(`http://hp-api.herokuapp.com/api/characters/house/${house}`)
+  return fetch(`https://hp-api.herokuapp.com/api/characters/house/${house}`)
     .then((response) => response.json())
     .then((charactersResult) => {
       const characters = charactersResult.map((character) => {
